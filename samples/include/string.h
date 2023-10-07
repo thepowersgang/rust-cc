4f2ee5f6a10b6676dd193145bec576cb4cad9531
John Hodge <tpg@mutabah.net>
2023-10-07 10:15:18 +0800

    include - add `memcmp`

--- samples/include/string.h
+++ samples/include/string.h
@@ -10,6 +10,7 @@ extern char *strncpy(char *dest, const char *src, size_t n);
 extern void *memcpy(void *dest, const void *src, size_t n);
 extern void *memset(void *s, int c, size_t n);
 extern void *memmove(void *dest, const void *src, size_t n);
+extern int memcmp(const void *s1, const void *s2, size_t n);
 
 extern char *strdup(const char *s);
 // POSIX >=200809
