d2db3823e3f971c4f4627ff6ec32829a7a161a52
John Hodge <tpg@mutabah.net>
2023-12-22 17:22:15 +0800

    mmir_opt - Dead writes

--- tools/mmir_opt/src/optimise.rs
+++ tools/mmir_opt/src/optimise.rs
@@ -2,7 +2,7 @@ use crate::mir::Terminator;
 use crate::mir::Statement;
 use crate::mir::Value;
 
-pub fn optimise_function(fcn: &mut crate::mir::Function, sig: &crate::types::FcnTy)
+pub fn optimise_function(fcn: &mut crate::mir::Function, _sig: &crate::types::FcnTy)
 {
     loop {
         let mut changed = false;
@@ -23,7 +23,6 @@ pub fn optimise_function(fcn: &mut crate::mir::Function, sig: &crate::types::Fcn
             changed = true;
         }
         // - Unused writes
-        #[cfg(false_)]
         while remove_dead_writes(fcn) {
             changed = true;
         }
@@ -283,6 +282,279 @@ fn const_propagate(fcn: &mut crate::mir::Function) -> bool
         },
         }
     }
+    rv
+}
+
+fn remove_dead_writes(fcn: &mut crate::mir::Function) -> bool
+{
+    // Identify writes to variables that are never read, just overwritten
+    // - This catches `"uninit"` assignments, AND just dead statements
+
+    // Method:
+    // - Enumerate paths through the function, stopping on a loop-back
+    struct Path {
+        ents: Vec<usize>,
+        looped: Option<usize>,
+    }
+    let paths = {
+        let mut paths = Vec::new();
+        let mut stack = Vec::new();
+        stack.push(vec![0usize]);
+        while let Some(v) = stack.pop() {
+            let mut check = |mut v: Vec<_>, blk: Option<usize>| {
+                //println!("{:?}, {:?}", v, blk);
+                let completed = if let Some(blk) = blk {
+                    v.iter().find(|b| **b == blk).is_some()
+                } else {
+                    true
+                };
+                if completed {
+                    paths.push(Path { ents: v, looped: blk });
+                }
+                else {
+                    if let Some(blk) = blk {
+                        v.push(blk);
+                    }
+                    stack.push(v);
+                }
+            };
+            let bb = &fcn.blocks[*v.last().unwrap()];
+            match bb.terminator {
+            Terminator::Invalid => { check(v, None); },
+            Terminator::Return => { check(v, None); },
+            Terminator::Diverge => { check(v, None); },
+            Terminator::Goto(blk) => {
+                check(v, Some(blk));
+                },
+            Terminator::Call(ref call) => {
+                check(v.clone(), Some(call.bb_ret));
+                check(v, Some(call.bb_panic));
+            },
+            Terminator::If(_, bb_true, bb_false) => {
+                check(v.clone(), Some(bb_true));
+                check(v, Some(bb_false));
+            },
+            }
+        }
+        paths
+    };
+
+    // - Enumerate reads/writes of each variable (locations)
+    //  > For each write, see if there is a read before the next write (along the path)
+    #[derive(PartialEq, Eq, PartialOrd, Ord, Debug)]
+    enum Op {
+        Read,
+        Write,
+    }
+    use ::std::collections::BTreeMap;
+    let mut ops = BTreeMap::<usize,Vec<(usize,usize,Op)>>::new();
+    for (bb_idx,bb) in fcn.blocks.iter().enumerate()
+    {
+        fn visit_slot_in_param(param: &crate::mir::Param, mut fcn: impl FnMut(&crate::mir::Slot)) {
+            if let crate::mir::Param::Slot(s) = param {
+                fcn(s);
+            }
+        }
+        fn visit_slot_in_value(value: &Value, mut fcn: impl FnMut(&crate::mir::Slot)) {
+            match value {
+            Value::Constant(_) => {},
+            Value::Use(v) => { fcn(v); },
+            Value::Borrow(_, v) => { fcn(v); },
+            Value::BinOp(a, _, b) => {
+                visit_slot_in_param(a, &mut fcn);
+                visit_slot_in_param(b, &mut fcn);
+            },
+            Value::UniOp(_, a) => {
+                fcn(a);
+            },
+            Value::Cast(a, _) => { fcn(a) },
+            Value::DstPtr(a) => { fcn(a) },
+            Value::DstMeta(a) => { fcn(a) },
+            Value::Tuple(ents) => {
+                for p in ents {
+                    visit_slot_in_param(p, &mut fcn);
+                }
+            },
+            Value::Array(_) => todo!(),
+            Value::Struct(_, _) => todo!(),
+            Value::UnionVariant(_, _, _) => todo!(),
+            Value::EnumVariant(_, _, _) => todo!(),
+            }
+        }
+
+        fn visit_read(ops: &mut BTreeMap<usize,Vec<(usize,usize,Op)>>, slot: &crate::mir::Slot, bb_idx: usize, stmt_idx: usize) {
+            for w in &slot.wrappers {
+                if let crate::mir::SlotWrapper::Index(i) = *w {
+                    ops.entry(i).or_default().push((bb_idx,stmt_idx, Op::Read));
+                }
+            }
+            if let crate::mir::SlotRoot::Local(i) = slot.root {
+                ops.entry(i).or_default().push((bb_idx,stmt_idx, Op::Read));
+            }
+        }
+        fn visit_write(ops: &mut BTreeMap<usize,Vec<(usize,usize,Op)>>, slot: &crate::mir::Slot, bb_idx: usize, stmt_idx: usize) {
+            for w in &slot.wrappers {
+                if let crate::mir::SlotWrapper::Index(i) = *w {
+                    ops.entry(i).or_default().push((bb_idx,stmt_idx, Op::Read));
+                }
+            }
+            if let Some(i) = slot.is_local() {
+                ops.entry(i).or_default().push((bb_idx,stmt_idx, Op::Write));
+            }
+        }
+        for (stmt_idx, stmt) in bb.statements.iter().enumerate() {
+            match stmt {
+            Statement::SpanComment(_) => {},
+            Statement::Assign(dst, src) => {
+                // Look for usage
+
+                visit_slot_in_value(src, |slot| {
+                    visit_read(&mut ops, slot, bb_idx, stmt_idx);
+                });
+
+                visit_write(&mut ops, dst, bb_idx, stmt_idx);
+            }
+            }
+        }
+        let stmt_idx = bb.statements.len();
+        match bb.terminator {
+        Terminator::Invalid => {},
+        Terminator::Return => {},
+        Terminator::Diverge => {},
+        Terminator::Goto(_) => {},
+        Terminator::Call(ref call) => {
+
+            for p in &call.args {
+                visit_slot_in_param(p, |slot| {
+                    visit_read(&mut ops, slot, bb_idx, stmt_idx);
+                });
+            }
+            
+            visit_write(&mut ops, &call.dst, bb_idx, stmt_idx);
+        }
+        Terminator::If(ref slot, _, _) => {
+            visit_read(&mut ops, slot, bb_idx, stmt_idx);
+        }
+        }
+    }
+
+    for (_,o) in ops.iter_mut() {
+        o.sort();
+    }
+    let mut rv = false;
+    for (i, ops) in &ops {
+        //println!("{i} {ops:?}");
+        let check_block = |wr_bb_idx: usize, wr_stmt_idx: usize, _i: usize, bb_idx: usize| {
+            for v @ &(bb,stmt,ref op) in ops.iter() {
+                // Same BB and variable index
+                if bb == bb_idx {
+                    if bb_idx == wr_bb_idx {
+                        if stmt < wr_stmt_idx {
+                            return Some(v);
+                        }
+                        else if let (Op::Read,true) = (op, stmt == wr_stmt_idx) {
+                            return Some(v);
+                        }
+                        else {
+                        }
+                    }
+                    else {
+                        return Some(v);
+                    }
+                }
+                if bb > bb_idx {
+                    break;
+                }
+            }
+            None
+        };
+        let find_usage = |wr_bb_idx: usize, wr_stmt_idx: usize, i: usize| {
+            // Look for a read/write with the same BB but a higher statement index
+            if let Some(v) = ops.iter()
+                .find(|&&(bb,stmt,_)| bb == wr_bb_idx && stmt > wr_stmt_idx) {
+                return Some(v);
+            }
+            // Find this BB in the paths and then look for a write.
+            'outer: for p in &paths {
+                // Is this starting BB in this path?
+                let Some(start) = p.ents.iter().position(|bb| *bb == wr_bb_idx) else { continue };
+
+                //println!("> {:?}", &p.ents[start+1..]);
+                for bb_idx in p.ents[start+1..].iter().copied() {
+                    if let Some(rv) = check_block(wr_bb_idx, wr_stmt_idx, i, bb_idx) {
+                        return Some(rv);
+                    }
+                    if bb_idx == wr_bb_idx {
+                        continue 'outer;
+                    }
+                }
+                // If this path looped, then semi-recurse
+                // - Find the loop-back point, and visit all path entries without recursing again
+                if let Some(loop_idx) = p.looped {
+                    for p in &paths {
+                        let Some(start) = p.ents.iter().position(|bb| *bb == loop_idx) else { continue };
+            
+                        //println!(">> {:?}", &p.ents[start..]);
+                        for bb_idx in p.ents[start..].iter().copied() {
+                            if let Some(rv) = check_block(wr_bb_idx, wr_stmt_idx, i, bb_idx) {
+                                return Some(rv);
+                            }
+                            if bb_idx == wr_bb_idx {
+                                break;
+                            }
+                        }
+                    }
+                }
+
+            }
+            None
+        };
+        for &(bb_idx, stmt_idx, ref o) in ops {
+            //println!("_{i}: Write {}-{}", bb_idx, stmt_idx);
+            if let Op::Write = o {
+                match find_usage(bb_idx, stmt_idx, *i) {
+                None | Some((_,_,Op::Write)) => {
+                    let bb = &mut fcn.blocks[bb_idx];
+                    if stmt_idx == bb.statements.len() {
+                        let can_remove = match bb.terminator {
+                            Terminator::Call(ref call) => {
+                                match call.target {
+                                crate::mir::CallTarget::Path(_) => None,
+                                crate::mir::CallTarget::Intrinsic(ref name, _) => match &name[..] {
+                                    "uninit" => Some(call.bb_ret),
+                                    _ => None,
+                                    },
+                                crate::mir::CallTarget::Value(_) => None,
+                                }
+
+                            },
+                            _ => panic!(),
+                            };
+                        if let Some(target) = can_remove {
+                            println!("remove write at {},{} - terminator {:?}", bb_idx, stmt_idx, bb.terminator);
+                            bb.terminator = Terminator::Goto(target);
+                            rv = true;
+                        }
+                    }
+                    else {
+                        println!("remove write at {},{} - statement {:?}", bb_idx, stmt_idx, bb.statements[stmt_idx]);
+                        bb.statements[stmt_idx] = Statement::SpanComment("".into());
+                        rv = true;
+                    }
+                },
+                _ => {},
+                }
+            }
+        }
+    }
+
+    for bb in &mut fcn.blocks {
+        bb.statements.retain(|s| match s {
+            Statement::SpanComment(ref c) if c.len() == 0 => false,
+            _ => true,
+        });
+    }
+
     rv
 }
 
